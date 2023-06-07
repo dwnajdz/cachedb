5051fdc5959a09dee1344d39751d2a2fcf98f36a
wspirrat <dawidnajdz@gmail.com>
2023-06-07 23:51:32 +0200

    server add, useless thing delete

---
+++ server/client.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+int failedError(std::string message) {
+    std::cerr << "Failed to " << message << std::endl;
+    return 1;
+}
+
+int connectToServer(std::string serverIP, int port, int buffer_size, char *message) {
+    std::cout << "connecting " << serverIP << ':' << port << std::endl;
+
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        return failedError("create socket");
+    }
+
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    if (inet_pton(AF_INET, serverIP.c_str(), &(serverAddress.sin_addr)) <= 0) {
+        return failedError("connect, invalid address/address not supported address=()" + serverIP);
+    }
+
+    if (connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
+        return failedError("connect to the server");
+    }
+
+    int messageStatus = send(clientSocket, message, strlen(message), 0);
+    if (messageStatus < 0) {
+        return failedError("send data to the server");
+    }
+
+    char buffer[buffer_size];
+    memset(buffer, 0, buffer_size);
+    int receiveStatus = recv(clientSocket, buffer, sizeof(buffer)-1, 0);
+    if (receiveStatus < 0) {
+        return failedError("receive data from server");
+    }
+
+    std::cout << "Server response: \n" << buffer << std::endl;
+    close(clientSocket);
+    return 0;
+}
+
+int main() {
+    std::string serverIP = "127.0.0.1";
+    int port = 3333;
+
+    while (true) {
+        char *message;
+        std::cin >> message;
+        connectToServer(serverIP, port, 4096, message);
+    }
+    // char *message = "STORE key value";
+
+    // connectToServer(serverIP, port, 4096, message);
+}
---
+++ server/server.cpp
@@ -0,0 +1,70 @@
+#include <iostream>  // For input/output operations
+#include <sys/socket.h>  // For socket related functions
+#include <netinet/in.h>  // For address structures
+#include <unistd.h>  // For close function
+
+// port address of server
+#define PORT 3333
+// buffer size of message
+#define BUFFER_SIZE 10000
+// number of maximum connections query if server is busy
+#define BACKLOG 10
+
+int failedError(std::string message) {
+    std::cerr << "Failed to " << message << std::endl;
+    return 1;
+}
+
+int main(int argc, char const* argv[])
+{
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        return failedError("create socket");
+    }
+
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    int bindResult = bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (bindResult == -1) {
+        return failedError("bind socket");
+    }
+
+    int listenResult = listen(serverSocket, BACKLOG);
+    if (listenResult == -1) {
+        close(serverSocket);
+        return failedError("listen for connections");
+    }
+
+    while (true) {
+        struct sockaddr_in clientAddress;
+        socklen_t clientAddressSize = sizeof(clientAddress);
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressSize);
+        if (clientSocket == -1) {
+            failedError("accept connection");
+            continue;
+        }
+
+        char buffer[BUFFER_SIZE];
+        int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesRead == -1) {
+            close(clientSocket);
+            failedError("receive data from client");
+            continue;
+        }
+
+        std::cout << "Received data from client: " << buffer << std::endl;
+
+        const char* response = "accepted";
+        int bytesSent = send(clientSocket, response, sizeof(response), 0);
+        if (bytesSent == -1) {
+            close(clientSocket);
+            failedError("send data to client");
+            continue;
+        }
+
+        close(clientSocket);
+    }
+}
